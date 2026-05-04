73fce322791e92fdc1307fcf6d7274b4e75ba93f
Yasir Hussain <yasirbasumo@gmail.com>
2026-05-04 23:32:29 +0500

    yasirpersonal

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
       {/* Header */}
       <header className="bg-gradient-to-r from-blue-950 to-indigo-600 shadow-lg">
         <div className="max-w-4xl mx-auto px-4 py-8">
-          <h1 className="text-4xl font-bold text-white mb-2">Yasir NOtes</h1>
+          <h1 className="text-4xl font-bold text-white mb-2">Yasir Personal Notes</h1>
           <p className="text-blue-100">Use this webapp to make good notes</p>
         </div>
       </header>
